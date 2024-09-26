ac188fd1d4fb3be402fa22232248b1b3de368f5f
dprasant <dprasant@purdue.edu>
2024-09-26 16:56:59 -0400

    interface with additional capabilities done

--- Main.java
+++ Main.java
@@ -5,8 +5,25 @@ interface Animal {
     //default methods don't need to be overriden but can be and private methods can be added here 
     //interfaces cannot have instance vars but can have int NUMBER_OF_LEGS = 4; (implicitly public static final (static - shared by class final - cannot be changed))
 
+    //all interface methods (abstract, default, static) are implicitly public  
+
     void makeSound(); 
     void eat(); 
+
+    static void info() {
+        System.out.println("This is an animal interface");
+    }
+
+    int NUMBER_OF_LEGS = 4;
+
+    default void breathe() {
+        System.out.println("Animal is breathing");
+        internalMethod(); 
+    }
+
+    private void internalMethod() {
+        System.out.println("Internal logic for breathing");
+    }
 }
 
 class Dog implements Animal {
@@ -17,7 +34,7 @@ class Dog implements Animal {
 
     @Override
     public void makeSound() {
-        System.out.println("Dog says woof woof");
+        System.out.println("Dog says woof woof with " + NUMBER_OF_LEGS + " legs");
     }
 
     @Override
@@ -32,12 +49,17 @@ class Cat implements Animal {
 
     @Override
     public void makeSound() {
-        System.out.println("Cat says meow meow");
+        System.out.println("Cat says meow meow with " + NUMBER_OF_LEGS + " legs");
     }
 
     @Override
     public void eat() {
         System.out.println("Cat is eating");
+    }
+
+    @Override
+    public void breathe() {
+        System.out.println("Overriden method for cat breathing");
     }
 }
 
@@ -48,8 +70,13 @@ public class Main {
 
         dog.makeSound();
         dog.eat();
+        dog.breathe();
 
         cat.makeSound();
         cat.eat();
+        cat.breathe(); 
+
+        Animal.info(); 
+        System.out.println(Animal.NUMBER_OF_LEGS);
     }
 }
